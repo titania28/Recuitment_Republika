5f5ecb5adfc695922dbd68d268f88bc2bc5fe5b8
titania28 <titaniaputri28@gmail.com>
2017-04-21 19:42:25 -0700

    desain home

--- application/views/home.php
+++ application/views/home.php
@@ -1,33 +1,48 @@
-<div class="container">
-		<div class="bs-docs-example">
-		<br>
-		<br>
-		<h3>DAFTAR BERITA</h3>
-		<br>
-		<br>
-		<a href="<?php echo base_url();?>home/proses"><button type="submit" class="btn btn-default">Tambah Berita</button></a>
-            <table class="table">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Content</th>
-                  <th>Category</th>
-				  <th>Images</th>
-				  <th>Aksi</th>
-                </tr>
-              </thead>
-              <tbody>
-			  <?php foreach($news as $a) { ?>
-                <tr>
-                  <td><?php echo $a->title;?></td>
-                  <td><?php echo $a->content;?></td>
-				          <td><?php echo $a->kategori;?></td>
-				          <td><img src="<?php echo base_url();?>foto/<?php echo $a->image;?>" width="70" height="70" alt="img"></td>
-				          <td><a href="<?php echo base_url('home/batal/'.$a->id);?>" class = "btn btn-default btn-sm">Delete</a>
-                  <a href="<?php echo base_url('home/update/'.$a->id);?>" class = "btn btn-default btn-sm">Edit</a></td>
-                </tr>
-				<?php }?>
-              </tbody>
-            </table>
-          </div>
-		  </div>
+<!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">
+            <h3><i class="fa fa-angle-right"></i> Daftar Berita</h3>
+              <div class="row mt">
+                  <div class="col-md-12">
+                      <div class="content-panel">
+                          <table class="table table-striped table-advance table-hover">
+                             <div class="form-group">
+                              <div class="col-sm-10">
+                                  <input type="text" class="form-control" placeholder="Masukkan Kata Kunci">
+                              </div>
+                              <button type="button" class="btn btn-primary">Search</button>
+                          </div>
+                             <hr>
+                              <thead>
+                              <tr>
+                                  <th><i class="fa fa-bullhorn"></i> Title</th>
+                                  <th class="hidden-phone"><i class="fa fa-question-circle"></i> Content</th>
+                                  <th><i class="fa fa-bookmark"></i> Kategori</th>
+                                  <th><i class=" fa fa-edit"></i> Image </th>
+                                  <th><i class=" fa fa-bookmark"></i> Reporter </th>
+                                  <th></th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                               <?php foreach($news as $a) { ?>
+                              <tr>
+                                  <td><?php echo $a->title;?></td>
+                                   <td><?php echo $a->content;?></td>
+                                  <td><?php echo $a->kategori;?></td>
+                                  <td><img src="<?php echo base_url();?>foto/<?php echo $a->image;?>" width="70" height="70" alt="img"></td>
+                                  <td></td>
+                                  <td>
+                                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                                  </td>
+                              </tr>
+                              <?php }?>
+                              </tbody>
+                          </table>
+                      </div><!-- /content-panel -->
+                  </div><!-- /col-md-12 -->
+              </div><!-- /row -->
+
+      </section><! --/wrapper -->
+      </section><!-- /MAIN CONTENT -->
